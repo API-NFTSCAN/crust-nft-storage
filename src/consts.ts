1952a9cd31cda199e40633dba80c0e2c1cceb3aa
TonyCode2012 <TonyCode2012@126.com>
2021-11-29 14:28:00 +0800

    Adjust timeout; Roll back when failed;

--- src/consts.ts
+++ src/consts.ts
@@ -6,4 +6,4 @@ require('dotenv').config();
 export const seeds = process.env.CRUST_SEEDS as string;
 export const chainAddr = process.env.CHAIN_ADDR as string;
 export const port = process.env.SERVER_PORT as string;
-export const httpTimeout:number = 1800000
+export const httpTimeout:number = 180000
