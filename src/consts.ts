7d61a7cb81fd73500609051d11b0377c179f3b6c
TonyCode2012 <TonyCode2012@126.com>
2021-11-29 14:50:09 +0800

    Parameterize NFT list url

--- src/consts.ts
+++ src/consts.ts
@@ -6,4 +6,5 @@ require('dotenv').config();
 export const seeds = process.env.CRUST_SEEDS as string;
 export const chainAddr = process.env.CHAIN_ADDR as string;
 export const port = process.env.SERVER_PORT as string;
+export const NFTListUrl = process.env.NFT_LIST_URL as string;
 export const httpTimeout:number = 600000
--- src/utils/urlIterator.ts
+++ src/utils/urlIterator.ts
@@ -1,7 +1,5 @@
 import { httpGet } from './utils';
 
-export const baseUrl = 'https://nftscan.com/nftscan/nftSearch'
-
 export default class UrlIterator {
   private preUrls: string[];
   private url: string;
@@ -22,7 +20,7 @@ export default class UrlIterator {
     if (this.preUrls.length !== 0 ) {
       return true
     }
-    const reqUrl = `${baseUrl}?searchValue=${this.address}&pageIndex=${this.pageIndex}&pageSize=${this.pageSizeLimit}`
+    const reqUrl = `${this.url}?searchValue=${this.address}&pageIndex=${this.pageIndex}&pageSize=${this.pageSizeLimit}`
     const getRes = await httpGet(reqUrl)
     if (!getRes.status) {
       console.error(`Request ${reqUrl} failed`)
@@ -43,7 +41,7 @@ export default class UrlIterator {
   async nextUrls(): Promise<string[]> {
     let nftUrls = this.preUrls.splice(0, this.pageSize)
     while (nftUrls.length < this.pageSize) {
-      const reqUrl = `${baseUrl}?searchValue=${this.address}&pageIndex=${this.pageIndex}&pageSize=${this.pageSizeLimit}`
+      const reqUrl = `${this.url}?searchValue=${this.address}&pageIndex=${this.pageIndex}&pageSize=${this.pageSizeLimit}`
       const getRes = await httpGet(reqUrl)
       if (!getRes.status) {
         console.error(`Request ${reqUrl} failed`)
